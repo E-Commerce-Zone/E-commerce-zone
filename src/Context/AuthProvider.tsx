2f5fb5d9c55938e2ab8a4d4fcca9726acd9bce1d
mostafizur.rahaman <mostafizur.rahaman@trainingmug.com>
2023-03-13 23:30:15 +0600

    feat: auth provider setup done

--- src/Context/AuthProvider.tsx
+++ src/Context/AuthProvider.tsx
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
-import { createContext } from "vm";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, UserCredential } from "firebase/auth";
+import React, { useState, createContext } from "react";
+import auth from "../Config/Firebase.init";
 
 type AuthProviderProps = {
    children: React.ReactNode;
 };
+
+type AuthInfo = {
+   user: User | null;
+   setUser: React.Dispatch<React.SetStateAction<User | null>>; 
+   CreateUser: (email: string, password: string) => Promise<UserCredential>, 
+   SignIn: (email: string, password: string) => Promise<UserCredential>, 
+   UpdateUserProfile: (profile: {
+    displayName: string;
+    photoURL: string;
+}) => Promise<void>
+};
+
 type User = {
    uid: string;
    email: string;
 };
 
-type AuthInfo  = {
-   user: User | null; 
-}
-export const AuthContext = createContext();
+export const AuthContext = createContext({} as AuthInfo);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
    const [user, setUser] = useState<null | User>(null);
+   const CreateUser = (email:string,password:string ) =>{
+      return createUserWithEmailAndPassword(auth, email, password)
+   }  
 
-   const authInfo :AuthInfo= {user};
+   const SignIn = (email:string, password:string) => {
+         return signInWithEmailAndPassword(auth, email, password);
+   }
+
+   const UpdateUserProfile = (profile:{displayName:string, photoURL:string}) => {
+      return updateProfile(auth.currentUser!, profile);
+   }
+
+
+   
+   const authInfo: AuthInfo = { user,setUser, CreateUser, SignIn, UpdateUserProfile };
 
    return (
       <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
@@ -25,3 +48,4 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
 };
 
 export default AuthProvider;
+ 
