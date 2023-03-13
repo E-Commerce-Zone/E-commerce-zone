e33b8e28bad863a9f0031730c7038d94ec57d3a0
thohidul.islam <thohidul.islam@trainingmug.com>
2023-03-14 00:41:13 +0600

    change home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 
 const Home = () => {
-<<<<<<< HEAD
   return (
     <div>
       <h2> This is our home page. </h2>
+      <h4>This from my side = Thohid</h4>
+      <h3>Mostafiuzr Rahaman's line this</h3>
     </div>
   );
-=======
-   return (
-      <div>
-         <h2> This is our home page. </h2>
-         <h3>Mostafiuzr Rahaman's line this</h3>
-      </div>
-   );
->>>>>>> 282ed006256dac6f0407afe0500d77e3c1eeb0b2
 };
 
 export default Home;
