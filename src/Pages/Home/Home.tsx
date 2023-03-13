873abd5d033ccaae2d0091c6cd4496d86f394b23
thohidul.islam <thohidul.islam@trainingmug.com>
2023-03-14 00:21:01 +0600

    fate: change in home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-   return (
-      <div>
-         <h2> This is our home page. </h2>
-      </div>
-   );
+  return (
+    <div>
+      <h2> This is our home page. </h2>
+      <h4>This from my side- Thohid</h4>
+    </div>
+  );
 };
 
 export default Home;
